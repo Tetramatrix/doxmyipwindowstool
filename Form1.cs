e3cff3b134762242c044dd092865da596ec90b91
Phpdevpad <chibox@gmail.com>
2016-08-01 18:24:44 +0200

    inital release

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace IP2Location_Lite_IP_Browser
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("http://localhost:1972/main.html");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+
+            //toolStripContainer1.ContentPanel.Controls.Add(web_view);
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+  }
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SetProxy;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace IP2Location_Lite_IP_Browser
+{
+    static class Program
+    {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", CharSet = System.Runtime.InteropServices.CharSet.Unicode, SetLastError = true)]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        static extern bool SetDllDirectory(string lpPathName);
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            int wsize = IntPtr.Size;
+            string libdir = (wsize == 4) ? "x86" : "x64";
+            string appPath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            SetDllDirectory(System.IO.Path.Combine(appPath, libdir));
+
+            //WinInetInterop.SetConnectionProxy("localhost:1101");
+            //Process.Start(appPath+"\\php7\\php.exe -S localhost:1972 -t "+appPath);
+
+            //http://stackoverflow.com/questions/9679375/run-an-exe-from-c-sharp-code
+            // Use ProcessStartInfo class
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            //startInfo.UseShellExecute = false;
+            startInfo.FileName = appPath+"\\php7\\php.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = "-S localhost:1972 -t \"" + appPath + "\"";
+            
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                //using (Process exeProcess = Process.Start(startInfo))
+                //{
+                //   exeProcess.WaitForExit();
+                //}
+
+                Process exeProcess = Process.Start(startInfo);
+
+            }
+            catch
+            {
+                // Log error.
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
